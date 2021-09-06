04bc250b38391864d7ae80abdc44770035e696d0
paulao28 <66639015+paulao28@users.noreply.github.com>
2021-09-06 20:09:33 +0200

    Add files via upload

---
+++ jason_list.js
@@ -0,0 +1,64 @@
+////////////LIST GIFS////////////////
+
+var modelos = [
+    {
+        type: "simples",
+        model: "01",
+        img: "https://i.postimg.cc/NF0bnxsy/gif-01.gif"
+    },
+    {
+        type: "simples",
+        model: "02",
+        img: "https://i.postimg.cc/NF0bnxsy/gif-01.gif"
+    },
+    {
+        type: "simples",
+        model: "03",
+        img: "https://i.postimg.cc/NF0bnxsy/gif-01.gif"
+    },
+    {
+        type: "simples",
+        model: "04",
+        img: "https://i.postimg.cc/NF0bnxsy/gif-01.gif"
+    },
+    {
+        type: "casal",
+        model: "05",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "06",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "07",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "casal",
+        model: "08",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "nome",
+        model: "09",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "nome",
+        model: "10",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "nome",
+        model: "11",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    },
+    {
+        type: "nome",
+        model: "12",
+        img: "https://i.postimg.cc/VLsgGX5v/01.gif"
+    }
+]
